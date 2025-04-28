6efa820dcfa5d3129fb2290b73fc72fdd932b772
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-04-28 10:54:18 +0100

    print function

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,16 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-	cards := []string{newCard(), newCard()}
+	cards := deck{newCard(), newCard()}
 	cards = append(cards, "Good morning")
 	// fmt.Println(cards)
 
-	for i, card := range cards {
-		fmt.Println(i,card)
-	}
+	// for i, card := range cards {
+	// 	fmt.Println(i,card)
+	// }
+	cards.print()
 }
 
 func newCard() string {
