256f7808787e2657afd3ef0c6a141232e7eae1db
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-04-29 06:05:00 +0100

    creating the cards functions

--- cards/main.go
+++ cards/main.go
@@ -3,16 +3,7 @@ package main
 // import "fmt"
 
 func main() {
-	cards := deck{newCard(), newCard()}
-	cards = append(cards, "Good morning")
-	// fmt.Println(cards)
-
-	// for i, card := range cards {
-	// 	fmt.Println(i,card)
-	// }
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
-}
-
-func newCard() string {
-	return "Hello"
 }
